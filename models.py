62a3ec25991d2aa5983cf4379eb57346210d7603
m-buschmann <snep114@gmail.com>
2024-02-23 18:12:48 +0100

    added function "find_sensitive_terms", with some interceptions that arent solved yet

--- models.py
+++ models.py
@@ -4,16 +4,36 @@ db = SQLAlchemy()
 
 
 
+"""class Term(db.Model):
+    __tablename__ = 'terms'
+    id = db.Column(db.Integer, primary_key=True)
+    term = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(5000))
+    language = db.Column(db.String(10), nullable=False, server_default='')
+    alternatives = db.relationship('AlternativeTerm', backref='term', lazy=True)
+    offensiveness_ratings = db.relationship('OffensivenessRating', backref='term', lazy=True)
+    appropriate_alternative_ratings = db.relationship('AlternativeRating', backref='term', lazy=True)
+   """
+class AlternativeTerm(db.Model):
+    __tablename__ = 'alternative_terms'
+    id = db.Column(db.Integer, primary_key=True)
+    original_term_id = db.Column(db.Integer, db.ForeignKey('terms.id'), nullable=False)
+    alternative_term_id = db.Column(db.Integer, db.ForeignKey('terms.id'), nullable=False)
+
 class Term(db.Model):
     __tablename__ = 'terms'
     id = db.Column(db.Integer, primary_key=True)
     term = db.Column(db.String(100), unique=True)
     description = db.Column(db.String(5000))
     language = db.Column(db.String(10), nullable=False, server_default='')
-    alternatives = db.relationship('Term', backref='movie', lazy=True)
-    offensiveness_ratings = db.relationship('OffensivenessRating', backref='movie', lazy=True)
-    appropriate_alternative_ratings = db.relationship('AlternativeRating', backref='movie', lazy=True)
-   
+    # Define a dynamic relationship for alternatives
+    alternative_terms = db.relationship('AlternativeTerm',
+                                        primaryjoin=id==AlternativeTerm.original_term_id,
+                                        secondaryjoin=id==AlternativeTerm.alternative_term_id,
+                                        secondary='alternative_terms',
+                                        backref=db.backref('original_term', lazy='dynamic'),
+                                        lazy='dynamic')
+
 class OffensivenessRating(db.Model):
     __tablename__ = 'offensiveness_ratings'
     id = db.Column(db.Integer, primary_key=True)
@@ -27,8 +47,9 @@ class AlternativeRating(db.Model):
     alternative_term_id = db.Column(db.Integer, db.ForeignKey('terms.id'), nullable=False)
     rating = db.Column(db.Integer)
 
-class AlternativeTerm(db.Model):
+"""class AlternativeTerm(db.Model):
     __tablename__ = 'alternative_terms'
     id = db.Column(db.Integer, primary_key=True)
     original_term_id = db.Column(db.Integer, db.ForeignKey('terms.id'), nullable=False)
     alternative_term_id = db.Column(db.Integer, db.ForeignKey('terms.id'), nullable=False)
+"""
--- textchecker.py
+++ textchecker.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 from sqlalchemy import func, desc, select
+import nltk
+from nltk.stem import SnowballStemmer
 
 import os
 
@@ -34,9 +36,46 @@ def home():
     # render home.html template
     return render_template("home.html")
 
+# Map ISO language codes to SnowballStemmer's expected full language names
+language_map = {'de': 'german', 'en': 'english'}
+
+def find_sensitive_terms(text, language_code='de'):
+    # Convert ISO language code to SnowballStemmer's expected language name
+    stemmer_language = language_map.get(language_code)
+
+    # Initialize the stemmer based on the resolved language name
+    stemmer = SnowballStemmer(stemmer_language)
+
+    # Normalize and tokenize the input text
+    words = nltk.word_tokenize(text.lower())
+    stemmed_words = [stemmer.stem(word) for word in words]
+
+    sensitive_indices = []
+    sensitive_terms = []
+
+    for index, word in enumerate(stemmed_words):
+        # consider the language when filtering terms
+        term = Term.query.filter(Term.language == language_code, func.lower(Term.term) == func.lower(word)).first()
+        if term:
+            sensitive_indices.append(index)
+            sensitive_terms.append(term.term)  # Returns the term as stored in the database, is that what we want?
+
+    # check that everything works:
+    print(sensitive_indices, sensitive_terms)
+    return sensitive_indices, sensitive_terms
+
+
 @app.route('/submit', methods=['POST'])
 def submit():
     user_text = request.form['user_text']
+    # when we have the language button, we can use this line to get the language
+    #language = request.form.get('language', 'german') # default to german if no language set
+    
+    # Find sensitive terms in the user text
+    indices, terms = find_sensitive_terms(user_text) #, language)
+    
+    # when we have the html, we can use this line to return the results
+    #return render_template("results.html", user_text=user_text, indices=indices, terms=terms)
     return render_template("home.html", user_text=user_text)
 
 if __name__ == '__main__':
